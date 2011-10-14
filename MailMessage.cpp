9813d8eb2508949e58cef0efd7150aae9662e760
Juan V. Guerrero <ryoma.nagare@gmail.com>
2011-10-14 15:57:08 -0500

    Moving parser from MIME to IMF

--- MailMessage.cpp
+++ MailMessage.cpp
@@ -7,9 +7,9 @@
 //
 
 #include <iostream>
-#include "MailMessage.h"
 #include "MTLogger.h"
-#include "libetpan/mailmime.h"
+#include "libetpan/libetpan.h"
+#include "MailMessage.h"
 
 namespace pmm {
 	MailMessage::MailMessage(){
@@ -25,18 +25,41 @@ namespace pmm {
 		from = m.from;
 		subject = m.subject;
 	}
-
+	
 	void MailMessage::parse(MailMessage &m, const std::string &rawMessage){
 		size_t indx;
-		struct mailmime *result;
-		mailmime_parse(rawMessage.c_str(), rawMessage.size(), &indx, &result);
+		struct mailimf_message *result;
+		mailimf_message_parse(rawMessage.c_str(), rawMessage.size(), &indx, &result);
 		
-
-		for (clistiter *iter = clist_begin(result->mm_mime_fields->fld_list); iter != clist_end(result->mm_mime_fields->fld_list); iter = iter->next) {
-			struct mailmime_field *field = (struct mailmime_field *)clist_content(iter);
-			pmm::Log << field->fld_data.fld_id << pmm::NL;
+		
+		for (clistiter *iter = clist_begin(result->msg_fields->fld_list); iter != clist_end(result->msg_fields->fld_list); iter = iter->next) {
+			struct mailimf_field *field = (struct mailimf_field *)clist_content(iter);
+			pmm::Log << field->fld_type << pmm::NL;
+			switch (field->fld_type) {
+				case MAILIMF_FIELD_FROM:
+				{
+					clistiter *iter2 = clist_begin(field->fld_data.fld_from->frm_mb_list->mb_list);
+					struct mailimf_mailbox *mbox = (struct mailimf_mailbox *)clist_content(iter2);
+					if(mbox->mb_display_name == NULL) m.from = mbox->mb_addr_spec;
+					else m.from = mbox->mb_display_name;
+#ifdef DEBUG
+					pmm::Log << "DEBUG: From=\"" << m.from << "\"" << pmm::NL;
+#endif
+				}
+					break;
+				case MAILIMF_FIELD_SUBJECT:
+				{
+					m.subject = field->fld_data.fld_subject->sbj_value;
+#ifdef DEBUG
+					pmm::Log << "DEBUG: Subject=\"" << m.subject << "\"" << pmm::NL;
+#endif
+				}
+					break;
+				default:
+					break;
+			}
+			
 		}
-		
-		mailmime_free(result);
+		mailimf_message_free(result);
 	}
 }
