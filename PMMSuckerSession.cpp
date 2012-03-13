21241d77a9bbbfc9c3d8eb24b0564a3b22e3fb1d
Juan V. Guerrero <ryoma.nagare@gmail.com>
2012-03-13 09:18:02 -0500

    Removing forced encoding on POSTs

--- PMMSuckerSession.cpp
+++ PMMSuckerSession.cpp
@@ -265,6 +265,7 @@ namespace pmm {
 		curl_easy_setopt(www, CURLOPT_WRITEDATA, buffer);
 		curl_easy_setopt(www, CURLOPT_WRITEFUNCTION, gotDataFromServer);
 		curl_easy_setopt(www, CURLOPT_FAILONERROR, 1);
+		//curl_easy_setopt(www, CURLOPT_ENCODING, "utf-8");
 		std::stringstream encodedPost;
 		std::map<std::string, std::string>::iterator keypair;
 		for (keypair = postData.begin(); keypair != postData.end(); keypair++) {
@@ -299,7 +300,6 @@ namespace pmm {
 		preparePostRequest(www, postData, &serverOutput);
 		//Let's url encode the param map
 		curl_easy_setopt(www, CURLOPT_ERRORBUFFER, errorBuffer);
-		//curl_easy_setopt(www, CURLOPT_ENCODING, "utf-8");
 		CURLcode ret = curl_easy_perform(www);
 		if(ret == CURLE_OK){
 			output.assign(serverOutput.buffer, serverOutput.size);
