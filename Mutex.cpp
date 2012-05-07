5746b5a8b4598720377cc74bc6627c61ccd5b57d
Juan V. Guerrero <ryoma.nagare@gmail.com>
2012-05-07 15:20:59 -0500

    Now using error check mutexes

--- Mutex.cpp
+++ Mutex.cpp
@@ -7,13 +7,22 @@
 //
 
 #include <iostream>
+#include <cerrno>
 #include <pthread.h>
 #include "Mutex.h"
 
 namespace pmm {
 	
 	Mutex::Mutex(){
-		pthread_mutex_init(&theMutex, NULL);
+		pthread_mutexattr_init(&attrs);
+#ifdef __linux__
+		pthread_mutexattr_settype(&attrs, PTHREAD_MUTEX_ERRORCHECK_NP);
+#else
+		pthread_mutexattr_settype(&attrs, PTHREAD_MUTEX_ERRORCHECK);
+#endif
+		pthread_mutex_init(&theMutex, &attrs);
+		lFile = NULL;
+		lLine = -1;
 	}
 	
 	Mutex::~Mutex() {
@@ -21,11 +30,49 @@ namespace pmm {
 	}
 	
 	void Mutex::lock() {
+#ifdef USE_REGULAR_MUTEXES
 		pthread_mutex_lock(&theMutex);
+#else
+		switch(pthread_mutex_trylock(&theMutex)){
+			case 0:
+				//Mutex locked succesfully
+				break;
+			case EBUSY:
+				if(lFile != NULL && lLine > 0) throw MutexLockException("Can't lock an already locked mutex!!!", lFile, lLine);
+				else throw MutexLockException("Can't lock an already locked mutex!!!");
+				break;
+			case EINVAL:
+				throw MutexLockException("Unable to lock a non-properly-initialized mutex!!!");
+				break;
+			default:
+				throw MutexLockException("Can't lock this mutex!!!");
+		}
+#endif
+	}
+	
+	void Mutex::lockWithInfo(const char *_srcFile, int _srcLine){
+		lock();
+		lFile = (char *)_srcFile;
+		lLine = _srcLine;
 	}
 	
 	void Mutex::unlock() {
+		lFile = NULL;
+		lLine = -1;
 		pthread_mutex_unlock(&theMutex);
+	}
+	
+	MutexLockException::MutexLockException(){
+		
+	}
+	
+	MutexLockException::MutexLockException(const std::string &_errmsg) : pmm::GenericException(_errmsg){
+		
+	}
+	
+	MutexLockException::MutexLockException(const std::string &_errmsg, const char *_srcFile, int _srcLine) : pmm::GenericException(_errmsg){
+		srcFile = _srcFile;
+		srcLine = _srcLine;
 	}
 
 	AtomicFlag::AtomicFlag() : Mutex(){
--- Mutex.h
+++ Mutex.h
@@ -9,16 +9,30 @@
 #ifndef PMM_Sucker_Mutex_h
 #define PMM_Sucker_Mutex_h
 #include<pthread.h>
+#include "GenericException.h"
 namespace pmm {
 	//Encapsulates a POSIX threads mutual exclusion device
 	class Mutex {
 	private:
 		pthread_mutex_t theMutex;
+		pthread_mutexattr_t attrs;
+		char *lFile;
+		int lLine;
 	public:
 		Mutex();
 		~Mutex();
 		void lock();
+		void lockWithInfo(const char *_srcFile = __FILE__, int _srcLine = __LINE__);
 		void unlock();
+	};
+	
+	class MutexLockException : public GenericException {
+	public:
+		std::string srcFile;
+		int srcLine;
+		MutexLockException();
+		MutexLockException(const std::string &_errmsg);
+		MutexLockException(const std::string &_errmsg, const char *_srcFile, int _srcLine);
 	};
 	
 	class AtomicFlag : protected Mutex {
