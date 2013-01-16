bdec7e90e24ffe85895c0f451ee4ac1097224cf5
Juan V. Guerrero <ryoma.nagare@gmail.com>
2013-01-16 16:26:10 -0500

    * Reducing logging on upload operations * Increasing upload interval to 3 seconds, that way we can bluk-upload more data in a single operation

--- MessageUploaderThread.cpp
+++ MessageUploaderThread.cpp
@@ -55,13 +55,15 @@ namespace pmm {
 					}
 				}
 			}
+			sleep(2);
 #else
 			int msgs_sent = session->uploadMultipleNotificationMessages(pmmStorageQueue);
 #ifdef DEBUG
-			pmm::Log << msgs_sent << " messages uploaded to app engine." << pmm::NL;
+			if(msgs_sent > 0) pmm::Log << msgs_sent << " messages uploaded to app engine." << pmm::NL;
 #endif
+			sleep(3);
 #endif
-			sleep(2);
+
 		}
 	}
 }
